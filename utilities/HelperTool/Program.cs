5582ba0ee2b909ea524ddf745e672338a32e09fe
Krzysztof Koziarski <krzyhook@gmail.com>
2024-09-01 15:42:09 +0200

    added HelperTool

---
+++ utilities/HelperTool/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+if (args.Length > 0)
+{
+    foreach (var arg in args)
+    {
+        Console.WriteLine($"Argument={arg}");
+    }
+}
+else
+{
+    Console.WriteLine("No arguments");
+}
